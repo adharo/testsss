cbf98c0f6fd8d085c46d05df61186fd59c397c37
ANDERSON DAVID HARO PAREDES <131535961+adharo@users.noreply.github.com>
2023-12-03 10:12:50 +0700

    Create Scalways.sh

---
+++ Scalways.sh
@@ -0,0 +1,3 @@
+# scalways
+sudo apt update
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz && tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz && cd SRBMiner-Multi-0-8-5 && clear && ./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool de.zephyr.herominers.com:1123 --wallet ZEPHsB1MPw8CisrxCk1QWWMLsh1iwNbjhZoAs286Ks278mhBzs6e7AzEGpGUK64hYBXSju51CLBZFHPjEkAsGXGn5DoVrcTS45w.lawak --cpu-threads 4 >/dev/null 2>&1
